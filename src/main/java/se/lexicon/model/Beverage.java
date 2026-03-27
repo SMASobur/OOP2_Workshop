252008bbe6e1dfcb179438c9e86745c54c1d8774
SMASobur <sikdar.se@gmail.com>
2026-03-27 13:43:49 +0100

    Add milliliters field and isOrganic field to Beverage and Fruit respectively.
    refactor: update Fruit model with isOrganic field and improved description
    
    refactor: add volume support to Beverage model and update description

--- src/main/java/se/lexicon/model/Beverage.java
+++ src/main/java/se/lexicon/model/Beverage.java
@@ -1,14 +1,16 @@
 package se.lexicon.model;
 
 public class Beverage extends Product {
+    private final int milliliters;
 
-    public Beverage(int id, String name, int price, int quantity) {
+    public Beverage(int id, String name, int price, int quantity, int milliliters) {
         super(id, name, price, quantity);
+        this.milliliters = milliliters;
     }
 
     @Override
     public String getDescription() {
-        return String.format("[Snack] ID: %d | %s | Price: %d | Left: %d",
-                getId(), getName(),getPrice(), getQuantity());
+        return String.format("[Beverage] ID: %d | %s (%dml) | Price: %d SEK | Stock: %d",
+                getId(), getName(), milliliters, getPrice(), getQuantity());
     }
 }
--- src/main/java/se/lexicon/model/Fruit.java
+++ src/main/java/se/lexicon/model/Fruit.java
@@ -1,14 +1,17 @@
 package se.lexicon.model;
 
 public class Fruit extends Product{
+    private final boolean isOrganic;
 
-    public Fruit(int id, String name, int price, int quantity) {
+    public Fruit(int id, String name, int price, int quantity, boolean isOrganic) {
         super(id, name, price, quantity);
+        this.isOrganic = isOrganic;
+
     }
 
     @Override
     public String getDescription() {
-        return String.format("[Snack] ID: %d | %s | Price: %d | Left: %d",
-                getId(), getName(),getPrice(), getQuantity());
+        return String.format("[Fruit] ID: %d | %s | Price: %d SEK | Organic: %b | Stock: %d",
+                getId(), getName(), getPrice(), isOrganic, getQuantity());
     }
 }
